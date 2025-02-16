134fe21ca827fb051845f3b2648422aef625161c
tsukasakop <tsukasakop@gmail.com>
2025-02-17 05:17:27 +0900

    ➖ Use ft_strdup

--- src/command/get_path.c
+++ src/command/get_path.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:29:27 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/16 20:16:04 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/02/17 05:16:01 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,5 +22,5 @@
 const char	*get_path(const char *word)
 {
 	// TODO: resolve path if it has not slash
-	return (strdup(word));
+	return (ft_strdup(word));
 }
