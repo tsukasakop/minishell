c4ac39796135fa96c5f7cd723917fc29ec09440f
tsukasakop <tsukasakop@gmail.com>
2025-02-23 16:54:13 +0900

    ♻️ Impeove readability of code

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -6,17 +6,17 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 18:33:01 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/22 15:11:54 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/02/23 16:50:58 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
-int	builtin_exit(char **args)
+int	builtin_exit(char **argv)
 {
 	int	status;
 
-	if (args == NULL || args[0] == NULL || args[1] == NULL)
+	if (argv == NULL || argv[0] == NULL || argv[1] == NULL)
 		status = (int)get_exit_status();
 	else
 		status = ft_atoi(args[0]);
--- src/expand/get_exit_status_p.c
+++ src/expand/get_exit_status_p.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:40:13 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/21 18:28:24 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/02/23 16:53:52 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 /*
  * Function:
  * ----------------------------
+ *  Provide pointer where exit status on last command stored
  */
 unsigned char	*get_exit_status_p(void)
 {
