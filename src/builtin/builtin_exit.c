deea3adf96d44b9ca3ca9d91609d86b0ce527641
tsukasakop <tsukasakop@gmail.com>
2025-02-23 16:59:16 +0900

    🐛 Fix typo

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 18:33:01 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/23 16:50:58 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/02/23 16:58:07 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	builtin_exit(char **argv)
 	if (argv == NULL || argv[0] == NULL || argv[1] == NULL)
 		status = (int)get_exit_status();
 	else
-		status = ft_atoi(args[0]);
+		status = ft_atoi(argv[0]);
 	ft_fprintf(ft_stderr(), "exit\n");
 	exit(status);
 }
