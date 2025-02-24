8720a0e50319f8ac52ac88df99fa54d6c59c2f64
funa <orz.home35@gmail.com>
2025-02-24 16:59:06 +0900

    ✨ Minishell.hで項目ごとに整理

--- include/minishell.h
+++ include/minishell.h
@@ -6,14 +6,18 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 20:15:15 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/24 14:49:39 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/02/24 16:58:15 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
+
+/* header file*/
 # include <ft_stdio.h>
 # include <libft.h>
+
+/* library */
 # include <signal.h>
 # include <stdbool.h>
 # include <stdio.h>
@@ -26,10 +30,11 @@
 # include <sys/types.h>
 # include <sys/wait.h>
 
+/* macro */
 # define PROMPT "minishell$ "
-
 # define SHELL_NAME "minishell: "
 
+/* struct */
 typedef struct s_simple_cmd		t_simple_cmd;
 typedef struct s_redirect		t_redirect;
 typedef struct s_heredoc		t_heredoc;
@@ -62,52 +67,68 @@ struct				s_heredoc
 	t_heredoc		*next;
 };
 
+/* global variable */
 extern volatile unsigned char	g_signal;
 
-bool							init(void);
-void							close_fds_no_stdio(int *fds, size_t size);
-char							**pipe2simple_cmds(const char *pipe);
-bool							iterate_pipefd(bool is_first, bool is_last,
-									int (*stdio)[2], int *next_in);
-void							free_words(char **words);
-void							free_redirects(t_redirect *reds);
-void							resolve_redirects(int stdio[2],
-									t_redirect *red);
-void							connect_redirects_path(t_redirect *red);
-int								redirects_stdin(t_redirect *red);
-int								redirects_stdout(t_redirect *red);
-t_simple_cmd					*load_simple_cmd(char **cmds_text);
-t_simple_cmd					*fill_struct_simple_cmd(const char *text);
-void							expand_words(char **words);
-char							**fill_words(char **src, int wc);
-void							add_struct_heredoc(t_heredoc **here, \
-								char *eof, char *path);
-void							free_heredocs(t_heredoc *here);
-void							add_struct_redirect(t_redirect **reds, \
-								int type, char *path);
-bool							has_redirect(char *word);
-void							parse_redirects(t_redirect **reds, \
-								t_heredoc **here, char *word, char *path);
-const char						*get_path(const char *word);
-bool							execute_simple_cmd(const t_simple_cmd *scmd,
-									int stdio_fd[2], int next_in_fd,
-									char **envp);
-unsigned char					wait_status(void);
-void							free_simple_cmds(t_simple_cmd *scmds);
-unsigned char					eval_pipe(const char *text, char **envp);
-unsigned char					eval_text(const char *text, char **envp);
-void							set_exit_status(unsigned char st);
-int								main(int argc, char **argv, char **envp);
-void							flush_prompt(void);
-void							at_sigint(int signum);
-char							*get_input(void);
-void							at_sigint(int signum);
-void							set_handlers_default(void);
-void							set_handlers_for_process(void);
-void							set_handlers_for_prompt(void);
-void							set_signal(int signal);
-int								builtin_exit(char **args);
-unsigned char					get_exit_status(void);
-unsigned char					*get_exit_status_p(void);
-void							perror_exit(char *msg);
+/* builtin function */
+int				builtin_exit(char **argv);
+
+/* command function */
+const char		*get_path(const char *word);
+
+/* data function */
+void			add_struct_heredoc(t_heredoc **here, char *eof, char *path);
+void			add_struct_redirect(t_redirect **reds, int type, char *path);
+t_simple_cmd	*fill_struct_simple_cmd(const char *text);
+char			**fill_words(char **src, int wc);
+void			free_heredocs(t_heredoc *here);
+void			free_redirects(t_redirect *reds);
+void			free_simple_cmds(t_simple_cmd *scmds);
+void			free_words(char **words);
+bool			has_redirect(char *word);
+t_simple_cmd	*load_simple_cmd(char **cmds_text);
+void			parse_redirects(t_redirect **reds, t_heredoc **here, \
+								char *word, char *path);
+char			**pipe2simple_cmds(const char *pipe);
+
+/* expand function */
+void			expand_words(char **words);
+unsigned char	*get_exit_status_p(void);
+unsigned char	get_exit_status(void);
+void			set_exit_status(unsigned char st);
+
+/* main function */
+unsigned char	eval_pipe(const char *text, char **envp);
+unsigned char	eval_text(const char *text, char **envp);
+bool			execute_simple_cmd(const t_simple_cmd *scmd, int stdio_fd[2], \
+				int next_in_fd, char **envp);
+bool			init(void);
+int				main(int argc, char **argv, char **envp);
+
+/* pipe function */
+bool			iterate_pipefd(bool is_first, bool is_last, int (*stdio)[2], \
+				int *next_in);
+unsigned char	wait_status(void);
+
+/* read function */
+void			flush_prompt(void);
+char			*get_input(void);
+
+/* redirect function */
+void			connect_redirects_path(t_redirect *red);
+int				redirects_stdin(t_redirect *red);
+int				redirects_stdout(t_redirect *red);
+void			resolve_redirects(int stdio[2], t_redirect *red);
+
+/* signal function */
+void			at_sigint(int signal);
+void			set_handlers_default(void);
+void			set_handlers_for_process(void);
+void			set_handlers_for_prompt(void);
+void			set_signal(int signal);
+
+/* utils */
+void			close_fds_no_stdio(int *fds, size_t size);
+void			perror_exit(char *msg);
+
 #endif
