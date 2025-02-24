98ca9945d94c79761a2aa6a16766c33950268669
funa <orz.home35@gmail.com>
2025-02-25 02:29:26 +0900

    📝 Free_words関数の変更に伴い、free_simple_cmds関数の処理も修正

--- src/data/free_simple_cmds.c
+++ src/data/free_simple_cmds.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:32:33 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/22 18:19:55 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/02/25 02:23:18 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,6 @@ void	free_simple_cmds(t_simple_cmd *scmds)
 	{
 		tmp = scmds;
 		free_words(scmds->words);
-		free(scmds->words);
 		free_redirects(scmds->reds);
 		scmds = scmds->next;
 		free(tmp);
--- src/data/free_words.c
+++ src/data/free_words.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:26:32 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/25 02:19:15 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/02/25 02:23:46 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,17 +30,4 @@ void	free_words(char **words)
 		i++;
 	}
 	free(words);
-}
-
-int	main(int argc, char **argv, char **envp)
-{
-	(void)argc;
-	(void)argv;
-	(void)envp;
-
-	char *t = "12345 67890";
-	char **e = ft_split(t, ' ');
-	free_words(e);
-	// free(e);
-	return (0);
 }
