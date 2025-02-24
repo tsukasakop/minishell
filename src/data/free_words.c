7f8342e1842ce7fd6312451a4581fe63140196cd
funa <orz.home35@gmail.com>
2025-02-25 02:20:07 +0900

    🛠️ Free_words関数:引数で与えられた配列の先頭をfreeするよう修正

--- src/data/free_words.c
+++ src/data/free_words.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:26:32 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/24 13:53:07 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/02/25 02:19:15 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,28 @@
  */
 void	free_words(char **words)
 {
+	int	i;
+
+	i = 0;
 	if (words == NULL)
 		return ;
-	while (*words)
+	while (words[i])
 	{
-		free(*words);
-		words++;
+		free(words[i]);
+		i++;
 	}
-	//ToDO:wordsの先頭をfreeする
+	free(words);
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	(void)argc;
+	(void)argv;
+	(void)envp;
+
+	char *t = "12345 67890";
+	char **e = ft_split(t, ' ');
+	free_words(e);
+	// free(e);
+	return (0);
 }
