5f9b09ca6b453d7c4db6f2d85be179eb076cd24c
funa-yuy <150521318+funa-yuy@users.noreply.github.com>
2025-02-27 18:17:05 +0900

    chore: 変数名のリファクタリング (#19)
    * ✨ T_redirectのred_typeをredir_typeに変数名変更
    
    * ✨ ヒアドク関連の変数をリファクタリング
    
    * 🚧 確認用のprint_commands関数を一時的に追加
    
    * ✨ Words→e_cmdにした
    
    * ✨ 関数内の変数をリファクタリング
    
    * ✨ リダイレクト:red→redirに変更
    
    * ✨ S_simple_cmd:scmd→scmd_listに変更
    
    * ✨ Textをcmd_lineに変更
    
    * ✨ Norminette対応
    
    * 📝 テスト用関数を削除
    
    * ✨ S_cmd→scmdに。e_cmd→ecmdにした
    
    * ♻️ Pipe2simple_cmds関数の中に、fill_struct_simple_cmd関数を入れた
    
    * ♻️ Pipe2simple_cmds関数を、pipe2scmd_list関数に命名変更
    
    * ♻️ 変数名リファクタリング。hd_eof→eof&redir_type→type
    
    * ✨ 関数説明を修正した
    
    * 📝 Src/skelton.cを元に戻した

--- src/data/fill_struct_simple_cmd.c
+++ src/data/fill_struct_simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 22:33:55 by miyuu             #+#    #+#             */
-/*   Updated: 2025/02/22 22:34:16 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/02/27 16:28:52 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,40 +15,34 @@
 /*
  * Function:fill_struct_simple_cmd
  * ----------------------------
- * Parses const char *text and returns the head of the simple_cmd list.
+ * Parses const char *cmd_line and returns the head of the simple_cmd list.
  *
- * ToDO:norminetteエラー
  */
-t_simple_cmd	*fill_struct_simple_cmd(const char *text)
+t_simple_cmd	*fill_struct_simple_cmd(char **scmd_texts)
 {
-	char			**cmds_text;
+	t_simple_cmd	*new_scmd;
 	t_simple_cmd	*head;
-	t_simple_cmd	*current;
-	t_simple_cmd	*new_cmd;
+	t_simple_cmd	*cur;
 	int				i;
 
-	cmds_text = pipe2simple_cmds(text);
-	if (!cmds_text)
-		return (NULL);
 	i = 0;
 	head = NULL;
-	current = NULL;
-	while (cmds_text[i])
+	cur = NULL;
+	while (scmd_texts[i])
 	{
-		new_cmd = load_simple_cmd(ft_split(cmds_text[i], ' '));
-		if (!new_cmd)
+		new_scmd = load_simple_cmd(ft_split(scmd_texts[i], ' '));
+		if (!new_scmd)
 		{
 			free_simple_cmds(head);
-			free(cmds_text);
+			free(scmd_texts);
 			return (NULL);
 		}
 		if (!head)
-			head = new_cmd;
+			head = new_scmd;
 		else
-			current->next = new_cmd;
-		current = new_cmd;
+			cur->next = new_scmd;
+		cur = new_scmd;
 		i++;
 	}
-	free(cmds_text);
 	return (head);
 }
--- src/main/main.c
+++ src/main/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 20:04:20 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/19 15:46:41 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/02/26 16:09:18 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int argc, char **argv, char **envp)
 	while (true)
 	{
 		input = get_input();
-		last_status = eval_text(input, envp);
+		last_status = eval_cmd_line(input, envp);
 		set_exit_status(last_status);
 		free(input);
 	}
