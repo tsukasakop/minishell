321cbd66e7dee4d3959f93d37b4ad780ab9ee2bc
tsukasakop <tsukasakop@gmail.com>
2025-02-28 16:40:56 +0900

    🩹 Escape unused variable error

--- src/main/execute_on_current_env.c
+++ src/main/execute_on_current_env.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:30:10 by tkondo            #+#    #+#             */
-/*   Updated: 2025/02/28 16:30:21 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/02/28 16:40:20 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,9 @@ unsigned char	execute_on_current_env(char **ecmds, t_redirect *redir,
 {
 	unsigned char	status;
 
-	// TODO: fdを元に戻すために保存
-	// resolve_redirects((int [2]){0, 1}, redir);
+	// TODO: リダイレクト処理
+	(void)redir;
 	status = execute_builtin(ecmds, envp);
 	// TODO: セットしたfdを元に戻す
-	// restore_redirects(fd_backup);
 	return (status);
 }
