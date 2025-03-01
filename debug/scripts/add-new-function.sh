5b402347459ee590f9346ea5038d9fd74e3ccc0d
tsukasakop <tsukasakop@gmail.com>
2025-03-01 13:46:33 +0900

    🧑‍💻 Enable template

---
+++ debug/scripts/add-new-function.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+DEBUG_FILE="${DEBUG_FILE-/dev/null}"
+
+main()
+{
+    set -e
+    echo "Run ${0##*/} on bash $BASH_VERSION" > "$DEBUG_FILE"
+    # ここに処理を追加
+    ROOT_DIR="$(git rev-parse --show-toplevel)"
+    local DIR="src/${1%/*}"
+    local FILE="${1##*/}"
+    local NAME="${FILE%.c}"
+    local PATH="$DIR/$NAME.c"
+
+    echo "dir: $DIR" > "$DEBUG_FILE"
+    echo "name: $NAME" > "$DEBUG_FILE"
+    echo "path: $PATH" > "$DEBUG_FILE"
+    pwd > "$DEBUG_FILE"
+
+    /bin/mkdir -p "$DIR"
+    /bin/cat <<EOF >"$PATH"
+# include <minishell.h>
+
+/*
+ * Function: $NAME
+ * ----------------------------
+ */
+void	$NAME()
+{
+}
+EOF
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    main "$@"
+fi
+
