a1975508fb2db4ae893f49e3e2bd1b201accf75f
funa <orz.home35@gmail.com>
2025-03-02 19:04:46 +0900

    🚧 from_fdを、dup2でリダイレクトに繋げた。実行ままだできてない

--- src/redirect/redirects_stdin.c
+++ src/redirect/redirects_stdin.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 14:14:20 by miyuu             #+#    #+#             */
-/*   Updated: 2025/02/26 12:39:32 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/02 18:59:06 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,15 @@
  */
 int	redirects_stdin(t_redirect *redir)
 {
-	int	fd;
+	int	oldfd;
+	int	newfd;
 
-	fd = 0;
-	fd = open(redir->path, O_RDWR);
-	if (fd == -1)
+	oldfd = 0;
+	newfd = redir->from_fd;
+	oldfd = open(redir->path, O_RDWR);
+	if (oldfd == -1)
 		perror_exit((char *)redir->path);
-	if (dup2(fd, STDIN_FILENO) < 0)
+	if (dup2(oldfd, newfd) < 0)
 		perror_exit(NULL);
-	return (fd);
+	return (oldfd);
 }
