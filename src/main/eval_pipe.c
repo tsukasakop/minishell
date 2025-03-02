7afbe8e198cac7cbeb70d106356a356c116e978a
funa <orz.home35@gmail.com>
2025-03-02 18:16:36 +0900

    ✨ テスト用を削除&テストケース確認完了

--- src/main/eval_pipe.c
+++ src/main/eval_pipe.c
@@ -6,38 +6,18 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:33:15 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/02 16:28:11 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/02 18:13:44 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
-#include <stdio.h>
-void	print_commands(const t_simple_cmd *cmds)
-{
-	int i = 0;
 
-	while (cmds)
-	{
-		printf("\x1b[32mCommand[%d]:\n", i++);
-		printf("  ecmds:\x1b[39m");
-		for (int j = 0; cmds->ecmds[j]; j++)
-			printf(" \x1b[32m%s,\x1b[39m", cmds->ecmds[j]);
-		printf("\n\x1b[39m");
-		t_redirect *red = cmds->redir;
-		while (red)
-		{
-			printf("  \x1b[32mRedirect: type=%u, from_fd=%d, path=%s\x1b[39m\n", red->type, red->from_fd, red->path);
-			red = red->next;
-		}
-		cmds = cmds->next;
-	}
-}
 /*
  * Function:
  * ----------------------------
  * Execute pipeline
  *
- * const char* cmd_line: string of pipeline
+ * const char *cmd_line: string to do as a command
  * char **envp: string of envp
  */
 unsigned char	eval_pipe(const char *cmd_line, char **envp)
@@ -51,7 +31,6 @@ unsigned char	eval_pipe(const char *cmd_line, char **envp)
 	//ToDo:fill_struct_simple_cmdにheredocも渡す。
 	//fill_struct_simple_cmd(cmd_line, &scmd_list, &hd_list);になる
 	scmd_list = pipe2scmd_list(cmd_line);
-	print_commands(scmd_list);
 	hd_list = NULL;
 	if (!write_heredocs(hd_list))
 		return (0);
