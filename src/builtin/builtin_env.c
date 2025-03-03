11ed3ee9f2cd6b12e3b30bbe7f714d91d7d5238a
tsukasakop <tsukasakop@gmail.com>
2025-03-03 18:11:46 +0900

    🩹 Fix free target on env

--- src/builtin/builtin_env.c
+++ src/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:51:45 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/03 13:16:51 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/03/03 17:15:00 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,17 +20,19 @@
 int	builtin_env(char **argv)
 {
 	char	**envp;
+	char	**envp_bak;
 
 	(void)argv;
 	envp = ft_getenvp();
 	if (envp == NULL)
 		return (1);
+	envp_bak = envp;
 	while (*envp != NULL)
 	{
 		ft_printf("%s\n", *envp);
 		free(*envp);
 		envp++;
 	}
-	free(envp);
+	free(envp_bak);
 	return (0);
 }
