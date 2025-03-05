d9171cdf19b16d8780d4586a75284d335cc9dfe4
tsukasakop <tsukasakop@gmail.com>
2025-03-05 17:20:32 +0900

    ✅ Add testcase

--- debug/main/test_expand_single_token.c
+++ debug/main/test_expand_single_token.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 18:52:53 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/05 16:14:09 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/03/05 17:20:02 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,9 @@ static void	test_expand_single_token(char *name, char *value, char *token)
 	fprintf(stderr, " value = \"%s\"\n", value);
 	fprintf(stderr, " token = \"%s\"\n", token);
 	fprintf(stderr, "---------- Errors on Processing ----------\n");
-	ft_setenv(name, value, 1);
+	fprintf(stderr, " [%d]: from `ft_setenv(name, value, 1)`\n", ft_setenv(name,
+			value, 1));
+	fprintf(stderr, " [%s]: from `ft_getenv(name)`\n", ft_getenv(name));
 	pp = expand_single_token(token);
 	fprintf(stderr, "---------- Retuned Texts ----------\n");
 	print_str_array(pp);
@@ -50,6 +52,7 @@ int	main(void)
 	char	*name;
 	char	*value;
 
+	test_expand_single_token("a23", "a23$a23", "$a23");
 	test_expand_single_token("VAR", "   test  '  ", "'$VAR");
 	test_expand_single_token("VAR", "   test  '  ",
 		"aaa$VAR\"==$VAR===\"$VAR---\'$VAR\'+++");
