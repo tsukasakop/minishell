e057bab2e5499a8ea6a1f46f70d757eb4e979e2f
funa <orz.home35@gmail.com>
2025-03-05 14:28:56 +0900

    ✨ 少し修正

--- src/main/execute_simple_cmd.c
+++ src/main/execute_simple_cmd.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:30:10 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/05 14:06:36 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/05 14:25:42 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,6 @@ bool	execute_simple_cmd(const t_simple_cmd *scmd_list, int stdio_fd[2],
 	set_handlers_default();
 	close_fds_no_stdio(&next_in_fd, 1);
 	resolve_redirects(stdio_fd, scmd_list->redir);
-	// ToDo:e_cmd[0]がnullだった場合の処理を考える
 	if (!scmd_list->ecmds[0])
 		ft_exit(0);
 	if (is_builtin(scmd_list->ecmds[0]))
