74354bfbfc6679bb2ca4e450396d9119989ab97a
funa <orz.home35@gmail.com>
2025-03-06 04:44:27 +0900

    📝 一部修正

--- src/main/execute_on_current_env.c
+++ src/main/execute_on_current_env.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:30:10 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/06 03:52:19 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/06 04:40:45 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
  * ----------------------------
  * execute command on current env
  *
- * TODO: リダイレクト周りの機能が未実装
  */
 unsigned char	execute_on_current_env(char **ecmds, t_redirect *redir,
 		char **envp)
