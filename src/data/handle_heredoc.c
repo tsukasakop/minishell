967676fdfe5e2659b76a85afaa715c75ed442ff7
funa <orz.home35@gmail.com>
2025-03-15 03:04:17 +0900

    📝 タスク終了したTODOメモを削除

--- src/data/handle_heredoc.c
+++ src/data/handle_heredoc.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 14:45:35 by miyuu             #+#    #+#             */
-/*   Updated: 2025/03/15 01:58:42 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/15 02:45:26 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,6 @@ t_redirect	*handle_heredoc(char *eof, int from_fd)
 	if (!write_heredocs(eof, path))
 		return (NULL);
 	//TODO: write_heredocsが失敗した場合の処理を考える
-	//TODO: ヒアどく構造体は使わない、リダイレクト構造体はリターンする
 	redir = add_struct_redirect(REDIR_IN, from_fd, path);
 	return (redir);
 }
