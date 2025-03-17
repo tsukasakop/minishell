c4e47f1260c8bc311016878e858a3e1422f03ff9
tsukasakop <tsukasakop@gmail.com>
2025-03-17 22:26:22 +0900

    🎨 Break line to improve visibility

--- src/data/token2ecmds.c
+++ src/data/token2ecmds.c
@@ -1,4 +1,16 @@
-# include <minishell.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token2ecmds.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/17 20:36:46 by tkondo            #+#    #+#             */
+/*   Updated: 2025/03/17 20:48:41 by tkondo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
 
 /*
  * Function: token2ecmds
@@ -21,8 +33,9 @@ char	**token2ecmds(t_text_list *tokens)
 		if (expanded == NULL)
 			expanded = tmp;
 		else
-			expanded = (char **)null_terminated_array_join(
-					(void **)expanded, (void **)tmp);
+			expanded = (char **)null_terminated_array_join(\
+										(void **)expanded, \
+										(void **)tmp);
 		cur_token = cur_token->next;
 	}
 	if (expanded == NULL)
