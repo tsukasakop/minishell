84bdc1f424083da13230efe83d1942417fd38e49
funa <orz.home35@gmail.com>
2025-03-17 20:09:13 +0900

    🛠️ libft/src/ft_exec.c:PATHをunsetした場合の、パス検索処理を追加

--- libft/src/ft_exec.c
+++ libft/src/ft_exec.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 15:19:38 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/17 18:19:20 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/17 19:58:10 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,18 @@ static const char	*find_path(const char *name)
 	char	*tmp;
 	char	*path;
 	char	**dirs;
+	char	*cur_dir;
 
 	if (ft_getenv("PATH") == NULL)
-		return (NULL);
+	{
+		cur_dir = getcwd(NULL, 0);
+		if (!cur_dir)
+			return (NULL);
+		tmp = ft_strjoin(cur_dir, "/");
+		path = ft_strjoin(tmp, name);
+		free(cur_dir);
+		return (path);
+	}
 	dirs = ft_split(ft_getenv("PATH"), ':');
 	while (dirs && *dirs)
 	{
