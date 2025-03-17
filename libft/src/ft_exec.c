91fa051c147422c58df46265d4679adf81540946
funa <orz.home35@gmail.com>
2025-03-17 13:25:42 +0900

    🛠️ コマンド実行時のエラー処理を追加

--- libft/src/ft_exec.c
+++ libft/src/ft_exec.c
@@ -3,15 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_exec.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 15:19:38 by tkondo            #+#    #+#             */
-/*   Updated: 2025/01/04 15:30:16 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/03/17 02:30:57 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <_ft_unistd.h>
 #include <ft_stdlib.h>
+
+// void	command_not_found_handle(char *cmd)
+// {
+// 	char	*error_msg;
+
+// 	error_msg = ": command not found\n";
+// 	write(2, cmd, ft_strlen(cmd));
+// 	write(2, error_msg, ft_strlen(error_msg));
+// 	exit(127);
+// }
+
+// void	execute_cmd(char **cmd, char **envp)
+// {
+// 	if (access(cmd[0], F_OK) == -1)
+// 	{
+// 		if (ft_strchr (cmd[0], '/') == NULL)
+// 			command_not_found_handle(cmd[0]);
+// 		else
+// 			perror_exit(cmd[0], 127);
+// 	}
+// 	if (execve(cmd[0], cmd, envp) < 0)
+// 		perror_exit(cmd[0], 126);
+// }
 
 static const char	*find_path(const char *name)
 {
@@ -27,7 +50,7 @@ static const char	*find_path(const char *name)
 		tmp = ft_strjoin(*dirs, "/");
 		path = ft_strjoin(tmp, name);
 		free(tmp);
-		if (access(path, X_OK) == 0)
+		if (access(path, F_OK) == 0)
 		{
 			while (*dirs)
 				free(*dirs++);
@@ -49,7 +72,7 @@ int	ft_execvp(const char *path, char *const argv[])
 		return (execve(path, argv, NULL));
 	abs_path = find_path(path);
 	if (abs_path == NULL)
-		return (-1);
+		return (127);
 	ret = execve(abs_path, argv, NULL);
 	free((void *)abs_path);
 	return (ret);
