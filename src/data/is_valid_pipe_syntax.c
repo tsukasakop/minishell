0409369e72825b7874eea0e24314db00d5613064
funa <orz.home35@gmail.com>
2025-03-20 15:31:06 +0900

    ♻️ is_valid_pipe_syntax関数をリファクタリング中

--- src/data/is_valid_pipe_syntax.c
+++ src/data/is_valid_pipe_syntax.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 19:39:04 by miyuu             #+#    #+#             */
-/*   Updated: 2025/03/20 00:02:23 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/03/20 15:25:02 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,26 +20,26 @@
 bool	is_valid_pipe_syntax(const char *cmd_line)
 {
 	size_t	i;
-	bool	has_text;
+	size_t	token_len;
+	bool	expect_token;
 
 	i = 0;
-	has_text = true;
+	expect_token = false;
 	while (cmd_line[i])
 	{
-		i += skip_whitespace(&cmd_line[i]);
-		if (!cmd_line[i])
-			return (has_text);
-		has_text = false;
-		if (cmd_line[i] && cmd_line[i] != '|')
-			has_text = true;
-		i += get_tokenize_pipe_length(&cmd_line[i]);
+		i += len_between_ifs(&cmd_line[i]);
+		token_len = get_tokenize_pipe_length(&cmd_line[i]);
+		if (token_len == 0 && (expect_token || \
+							cmd_line[i] == '|'))
+			return (false);
+		i += token_len;
 		if (cmd_line[i] == '|')
 		{
-			if (!has_text)
-				return (false);
-			has_text = false;
 			i++;
+			expect_token = true;
 		}
+		else
+			expect_token = false;
 	}
-	return (has_text);
+	return (!expect_token);
 }
