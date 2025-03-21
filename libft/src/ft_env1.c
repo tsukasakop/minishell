7daef977ffa340b0dc1a9885b4ebea31177e5e39
tsukasakop <tsukasakop@gmail.com>
2025-03-21 13:28:57 +0900

    ⬆️ Fix memory leak on ft_putenv

--- libft/src/ft_env1.c
+++ libft/src/ft_env1.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/04 11:19:40 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/03 17:25:31 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/03/21 13:26:20 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,7 @@ int	ft_putenv(char *string)
 	char	*sp;
 	char	*key;
 	char	*val;
+	int		ret;
 
 	sp = ft_strchr(string, '=');
 	if (sp == NULL)
@@ -60,18 +61,13 @@ int	ft_putenv(char *string)
 	ft_memcpy(key, string, sp - string);
 	key[sp - string] = '\0';
 	val = ft_strdup(sp + 1);
-	if (val == NULL)
-	{
-		free(key);
-		return (-1);
-	}
-	if (ft_setenv(key, val, 1) == -1)
-	{
-		free(key);
-		free(val);
-		return (-1);
-	}
-	return (0);
+	if (val == NULL || ft_setenv(key, val, 1) == -1)
+		ret = -1;
+	else
+		ret = 0;
+	free(key);
+	free(val);
+	return (ret);
 }
 
 int	ft_unsetenv(const char *name)
