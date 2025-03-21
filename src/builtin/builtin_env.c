cc34efd7ac43df89e9afc87d96434506e9e4880e
tsukasakop <tsukasakop@gmail.com>
2025-03-21 17:35:43 +0900

    🚸 Free envp

--- src/builtin/builtin_env.c
+++ src/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:51:45 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/21 15:13:49 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/03/21 17:13:15 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,18 @@
 int	builtin_env(char **argv)
 {
 	char	**envp;
+	size_t	i;
 
 	(void)argv;
-	envp = ft_g_mmadd(ft_getenvp());
+	envp = ft_getenvp();
 	if (envp == NULL)
 		return (1);
-	while (*envp != NULL)
+	i = 0;
+	while (envp[i] != NULL)
 	{
-		ft_printf("%s\n", *envp);
-		envp++;
+		ft_printf("%s\n", envp[i]);
+		i++;
 	}
+	free_null_terminated_array((void **)envp);
 	return (0);
 }
