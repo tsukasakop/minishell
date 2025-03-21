270c70d746d85fed63c7f2784fa08941261d34c2
tsukasakop <tsukasakop@gmail.com>
2025-03-21 15:15:00 +0900

    ➕ Apply memory managet to ft_getenvp

--- src/builtin/builtin_env.c
+++ src/builtin/builtin_env.c
@@ -6,7 +6,7 @@
 /*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 12:51:45 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/20 15:03:11 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/03/21 15:13:49 by tkondo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ int	builtin_env(char **argv)
 	char	**envp;
 
 	(void)argv;
-	envp = ft_getenvp();
+	envp = ft_g_mmadd(ft_getenvp());
 	if (envp == NULL)
 		return (1);
 	while (*envp != NULL)
