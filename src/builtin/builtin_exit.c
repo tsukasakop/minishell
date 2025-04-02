bfab2b9ba8f002749284280f4605ab991efe01c0
funa <orz.home35@gmail.com>
2025-04-03 03:00:27 +0900

    🛠️ exitの引数が不正な場合のエラー処理を追加

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tkondo <tkondo@student.42tokyo.jp>         +#+  +:+       +#+        */
+/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 18:33:01 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/03 18:09:02 by tkondo           ###   ########.fr       */
+/*   Updated: 2025/04/03 02:59:30 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,41 @@
  *
  *  TODO: handle errors
  */
+bool	is_numeric(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (str[0] == '+' || str[0] == '-')
+		i++;
+	while (str[i])
+	{
+		if (ft_isdigit(str[i]) == 0)
+			return (false);
+		i++;
+	}
+	return (true);
+}
+
 int	builtin_exit(char **argv)
 {
 	int	status;
 
+	ft_fprintf(ft_stderr(), "exit\n");
+	if (argv[0] != NULL && argv[1] != NULL)
+	{
+		ft_fprintf(ft_stderr(), "bash: exit: too many arguments\n");
+		return (1);
+	}
 	if (argv == NULL || argv[0] == NULL)
 		status = (int)get_exit_status();
+	else if (argv[0][0]== '\0' || !is_numeric(argv[0]))
+	{
+		status = 2;
+		ft_fprintf(ft_stderr(), "bash: exit: %s: numeric argument required\n", argv[0]);
+	}
 	else
 		status = ft_atoi(argv[0]);
-	ft_fprintf(ft_stderr(), "exit\n");
 	ft_exit(status);
 	return (1);
 }
