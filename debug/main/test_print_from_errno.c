373030afe1df19fe51f124c68cd622b68f0e4987
funa <orz.home35@gmail.com>
2025-04-06 03:41:35 +0900

    📝 エラー出力テスト用main関数内の関数を少し修正

--- debug/main/test_print_from_errno.c
+++ debug/main/test_print_from_errno.c
@@ -12,63 +12,68 @@
 
 #include <minishell.h>
 
-void	perrmsg_with_str(t_errmsg_type err_type, char *arg)
+void	perrmsg_with_str(t_errmsg_type err_type, char *str)
 {
 	ft_putstr_fd(SHELL_NAME, 2);
 	if (err_type == EM_SYSCALL)
 	{
-		perror(arg);
+		perror(str);
 		return ;
 	}
 	else if (err_type == EM_CD_SYSCALL)
 	{
 		ft_putstr_fd("cd: ", 2);
-		perror(arg);
+		perror(str);
 		return ;
 	}
 	else if (err_type == EM_AMBRDIR)
 	{
-		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(str, 2);
 		ft_putstr_fd(": ambiguous redirect", 2);
 	}
 	else if (err_type == EM_SYNTAX)
 	{
 		ft_putstr_fd("syntax error near unexpected token `", 2);
-		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(str, 2);
 		ft_putstr_fd("\'", 2);
 	}
 	else if (err_type == EM_ISDIR)
 	{
-		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(str, 2);
 		ft_putstr_fd(": Is a directory", 2);
 	}
 	else if (err_type == EM_CMDNFND)
 	{
-		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(str, 2);
 		ft_putstr_fd(": command not found", 2);
-	}
-	else if (err_type == EM_EXPO_BADID)
-	{
-		ft_putstr_fd("export: `", 2);
-		ft_putstr_fd(arg, 2);
-		ft_putstr_fd("\': not a valid identifier", 2);
 	}
 	else if (err_type == EM_HEREDOC)
 	{
 		ft_putstr_fd("warning: here-document delimited by end-of-file (wanted `", 2);
-		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(str, 2);
 		ft_putstr_fd("\')", 2);
 	}
 	else if (err_type == EM_MANYARG)
 	{
-		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(str, 2);
 		ft_putstr_fd(": too many arguments", 2);
+	}
+	else if (err_type == EM_EXPO_BADID)
+	{
+		ft_putstr_fd("export: `", 2);
+		ft_putstr_fd(str, 2);
+		ft_putstr_fd("\': not a valid identifier", 2);
+	}
+	else if (err_type == EM_EXIT_NONUM)
+	{
+		ft_putstr_fd("exit: ", 2);
+		ft_putstr_fd(str, 2);
+		ft_putstr_fd(": numeric argument required", 2);
 	}
 	else if (err_type == EM_CD_OPWDNSET)
 	{
 		ft_putstr_fd("cd: OLDPWD not set", 2);
 	}
-
 	ft_putstr_fd("\n", 2);
 }
 
