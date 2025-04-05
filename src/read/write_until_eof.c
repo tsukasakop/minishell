869c1d53956d26304fdcb33a628b61b5b17fdbe8
funa <orz.home35@gmail.com>
2025-04-05 22:36:32 +0900

    🚧 ヒアドクのエラー出力に、自作errnoからのエラー出力関数を適用した

--- src/read/write_until_eof.c
+++ src/read/write_until_eof.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/26 16:18:21 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/29 20:25:54 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/05 22:17:49 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,8 @@ void	write_until_eof(int fd, const char *raw_eof)
 		}
 		if (line == NULL)
 		{
-			ft_fprintf(ft_stderr(), ERR_HEREDOC, SHELL_NAME, hd_eof);
+			perrmsg_with_arg(EM_HEREDOC, (char *)hd_eof);
+			// ft_fprintf(ft_stderr(), ERR_HEREDOC, SHELL_NAME, hd_eof);
 			break ;
 		}
 		ft_fprintf(file, "%s\n", line);
---
+++ src/utils/perrmsg_with_arg.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   perrmsg_with_arg.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/05 22:14:31 by miyuu             #+#    #+#             */
+/*   Updated: 2025/04/05 22:21:45 by miyuu            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <minishell.h>
+
+void	perrmsg_with_arg(t_errmsg_type err_type, char *arg)
+{
+	ft_putstr_fd(SHELL_NAME, 2);
+	if (err_type == EM_SYSCALL)
+	{
+		perror(arg);
+		return ;
+	}
+	else if (err_type == EM_AMBRDIR)
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": ambiguous redirect", 2);
+	}
+	else if (err_type == EM_SYNTAX)
+	{
+		ft_putstr_fd("syntax error near unexpected token `", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd("\'", 2);
+	}
+	else if (err_type == EM_ISDIR)
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": Is a directory", 2);
+	}
+	else if (err_type == EM_CMDNFND)
+	{
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": command not found", 2);
+	}
+	else if (err_type == EM_EXPO_BADID)
+	{
+		ft_putstr_fd("export: `", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd("\': not a valid identifier", 2);
+	}
+	else if (err_type == EM_HEREDOC)
+	{
+		ft_putstr_fd("warning: here-document delimited by end-of-file (wanted `", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd("\')", 2);
+	}
+	else if (err_type == EM_CD_2MARG)
+	{
+		ft_putstr_fd("cd: too many arguments", 2);
+	}
+	else if (err_type == EM_CD_OPWDNSET)
+	{
+		ft_putstr_fd("cd: OLDPWD not set", 2);
+	}
+	else if (err_type == EM_CD)
+	{
+		ft_putstr_fd("cd: ", 2);
+		ft_putstr_fd(arg, 2);
+	}
+	ft_putstr_fd("\n", 2);
+}
