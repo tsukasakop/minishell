8b3d0b07abf48fa4a8a9eaa235916155e571c05a
funa <orz.home35@gmail.com>
2025-04-05 15:57:10 +0900

    ♻️ if分岐ないの書き方を修正

--- src/builtin/builtin_exit.c
+++ src/builtin/builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/21 18:33:01 by tkondo            #+#    #+#             */
-/*   Updated: 2025/04/03 20:10:54 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/05 15:51:15 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,14 @@ int	builtin_exit(char **argv)
 	int	status;
 
 	ft_fprintf(ft_stderr(), "exit\n");
-	if (is_numeric(argv[0]) == true && !(argv[1] == NULL))
+	if (is_numeric(argv[0]) && !(argv[1] == NULL))
 	{
 		ft_fprintf(ft_stderr(), "bash: exit: too many arguments\n");
 		return (1);
 	}
 	if (argv == NULL || argv[0] == NULL)
 		status = (int)get_exit_status();
-	else if (is_numeric(argv[0]) == false)
+	else if (!is_numeric(argv[0]))
 	{
 		ft_fprintf(ft_stderr(), \
 			"bash: exit: %s: numeric argument required\n", argv[0]);
