a79cb6bbbd9d942c1280c9cdbcf46e962ddf8e58
funa <orz.home35@gmail.com>
2025-04-09 15:32:34 +0900

    ♻️ eval_pipe関数を25行以内にした。また、コメントも削除した

--- src/main/eval_cmd_line.c
+++ src/main/eval_cmd_line.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:33:40 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/27 19:37:13 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/09 15:27:44 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,18 @@
  */
 unsigned char	eval_cmd_line(char **envp)
 {
-	char			*input;
-	unsigned char	status;
+	char				*input;
+	const t_simple_cmd	*scmd_list;
+	unsigned char		status;
 
 	set_error_type(NOERR);
 	input = get_input();
 	if (!input)
 		return (get_exit_status_from_err_type(get_error_type()));
-	status = eval_pipe(input, envp);
+	scmd_list = init_scmd_list(input);
+	if (!scmd_list)
+		return (get_exit_status_from_err_type(get_error_type()));
+	status = eval_pipe(scmd_list, envp);
 	if (g_signal)
 		status = 128 + g_signal;
 	return (status);
--- src/main/eval_pipe.c
+++ src/main/eval_pipe.c
@@ -6,7 +6,7 @@
 /*   By: miyuu <miyuu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 19:33:15 by tkondo            #+#    #+#             */
-/*   Updated: 2025/03/27 19:37:14 by miyuu            ###   ########.fr       */
+/*   Updated: 2025/04/09 15:29:09 by miyuu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,40 +20,29 @@
  * const char *cmd_line: string to do as a command
  * char **envp: string of envp
  */
-unsigned char	eval_pipe(const char *cmd_line, char **envp)
+unsigned char	eval_pipe(const t_simple_cmd *scmd_list, char **envp)
 {
-	const t_simple_cmd	*scmd_list;
 	t_simple_cmd		*cur;
 	int					stdio_fd[2];
 	int					next_in_fd;
 
-	set_error_type(NOERR);
-	scmd_list = init_scmd_list(cmd_line);
-	if (!scmd_list)
-		return (get_exit_status_from_err_type(get_error_type()));
-	// TODO: session = init_session();
 	stdio_fd[0] = STDIN_FILENO;
 	stdio_fd[1] = STDOUT_FILENO;
 	next_in_fd = STDIN_FILENO;
 	cur = (t_simple_cmd *)scmd_list;
-	// TODO: env = ENV_CHILD;
 	if (cur && cur->next == NULL && is_builtin(cur->ecmds[0]))
-		// TODO: env = ENV_PARENT;
 		return (execute_on_current_env(cur->ecmds, cur->redir, envp));
 	while (cur)
 	{
 		if (!iterate_pipefd(cur == scmd_list, cur->next == NULL, &stdio_fd, \
 			&next_in_fd))
 		{
-			// TODO: break;
 			close_fds_no_stdio(stdio_fd, 2);
 			close_fds_no_stdio(&next_in_fd, 1);
 		}
 		execute_simple_cmd(cur, stdio_fd, next_in_fd, envp);
 		cur = cur->next;
 	}
-	// TODO: del_pipe(pipe);
-	// TODO: ->free_simple_cmds((t_simple_cmd *)scmd_list);
 	close_fds_no_stdio((int [3]){stdio_fd[0], stdio_fd[1], next_in_fd}, 3);
 	return (wait_status());
 }
